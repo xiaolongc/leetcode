cd3dcc29081d4cec5116e4188257be88a2936f3b
ikaruga4600 <ikaruga@voidsun.com>
2023-02-13 17:04:25 +0800

    l3

---
+++ src/com/ik/top100/L3.java
@@ -0,0 +1,33 @@
+package com.ik.top100;
+
+public class L3 {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() <= 1) {
+            return s.length();
+        }
+        int maxLength = 1;
+        int length = 1;
+        int startIdx = 0;
+        int endIdx = 0;
+        int[] mapping = new int[128];
+        mapping[s.charAt(startIdx)] = 1;
+        while (endIdx < s.length() - 1) {
+            endIdx++;
+            mapping[s.charAt(endIdx)]++;
+            length++;
+            if (mapping[s.charAt(endIdx)] == 1) {
+                maxLength = Math.max(length, maxLength);
+                continue;
+            }
+            while (startIdx < endIdx) {
+                mapping[s.charAt(startIdx)]--;
+                startIdx++;
+                length--;
+                if (mapping[s.charAt(startIdx - 1)] == 1) {
+                    break;
+                }
+            }
+        }
+        return maxLength;
+    }
+}
