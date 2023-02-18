235cbaae36a2c0db442b18b5f85dd22538b939ec
xiaolongc <xiaolongc@yahoo.com>
2023-02-18 12:16:31 +0800

    removeElement

---
+++ src/com/art/top100/c0218/RemoveElement.java
@@ -0,0 +1,27 @@
+package com.art.top100.c0218;
+
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+//        int i = 0 ,j = 1;
+//        while (i<nums.length&&j<nums.length){
+//            if (nums[i]==val){
+//                if (nums[j]!=val){
+//                    nums[i] = nums[j];
+//                }
+//                j++;
+//            }
+//            i++;
+//        }
+//        return i+1;
+
+        if (nums==null || nums.length<1) return 0;
+        int j = 0;
+        for (int i=0;i<nums.length;i++){
+            if (nums[i]!=val){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
